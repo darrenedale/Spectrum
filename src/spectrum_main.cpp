324ca6eb4576632f906cd3b2bfb0ca17d2d45474
Darren Edale <darren@equituk.net>
2021-02-25 19:39:06 +0000

    Updated emulator entry point
    - now uses custom QApplication class (provides settings infrastructure)

--- src/spectrum_main.cpp
+++ src/spectrum_main.cpp
@@ -1,11 +1,9 @@
-#include <QApplication>
-
+#include "application.h"
 #include "mainwindow.h"
-
 
 int main( int argc, char * argv[] )
 {
-	QApplication app(argc, argv);
+	Spectrum::Application app(argc, argv);
     QApplication::quitOnLastWindowClosed();
 	Spectrum::MainWindow win;
 	win.show();
