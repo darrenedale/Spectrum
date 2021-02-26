6e03ff924691bbf0e461caf7e749d8f3aa815c5a
Darren Edale <darren@equituk.net>
2021-02-26 18:27:50 +0000

    Updated Cpu base class
    - fixed a type discrepancy
    - fixed some clang-tidy reports
    - reformatted for coding standards

--- src/cpu.h
+++ src/cpu.h
@@ -1,41 +1,50 @@
 #ifndef CPU_H
 #define CPU_H
 
-class Cpu {
-	public:
-		Cpu( unsigned char * mem, int memSize );
-		virtual ~Cpu( void );
+#include <cstdint>
 
-		void setMemory( unsigned char * mem, int memsize );
+class Cpu
+{
+public:
+    Cpu(std::uint8_t * mem, int memSize);
+    virtual ~Cpu();
 
-		inline unsigned char * memory( void ) {
-			return m_ram;
-		}
+    void setMemory(std::uint8_t * mem, int memsize);
 
-		inline int memorySize( void ) {
-			return m_ramSize;
-		}
+    inline std::uint8_t * memory()
+    {
+        return m_ram;
+    }
 
-		inline void setClockSpeed( unsigned long long hz ) {
-			m_clockSpeed = hz;
-		}
+    inline int memorySize() const
+    {
+        return m_ramSize;
+    }
 
-		inline void setClockSpeedMHz( double mhz ) {
-			m_clockSpeed = (unsigned long long)(mhz * 1000000);
-		}
+    inline void setClockSpeed(unsigned long long hz)
+    {
+        m_clockSpeed = hz;
+    }
 
-		inline unsigned long long clockSpeed( void ) {
-			return m_clockSpeed;
-		}
+    inline void setClockSpeedMHz(double mhz)
+    {
+        m_clockSpeed = static_cast<unsigned long long>(mhz * 1000000);
+    }
 
-		inline double clockSpeedMHz( void ) {
-			return double(m_clockSpeed) / 1000000.0;
-		}
+    inline unsigned long long clockSpeed() const
+    {
+        return m_clockSpeed;
+    }
 
-	protected:
-		unsigned char * m_ram;
-		int m_ramSize;
-		int m_clockSpeed;
+    inline double clockSpeedMHz() const
+    {
+        return static_cast<double>(m_clockSpeed) / 1000000.0;
+    }
+
+protected:
+    std::uint8_t * m_ram;
+    int m_ramSize;
+    unsigned long long m_clockSpeed;
 };
 
 #endif // CPU_H
