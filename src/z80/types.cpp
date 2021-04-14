6915730b426eaac2e7c1269475f9592d3926e007
Darren Edale <darren@equituk.net>
2021-04-14 17:40:01 +0100

    Updted Z80 types
    - made UDLs for Z80 8/16-bit values usable
    - added documentation
    - addition string representations for enumerated values
    - string representations for enums use std::string literal rather than implicitly invoking const char * constructor

--- src/z80/types.cpp
+++ src/z80/types.cpp
@@ -2,50 +2,54 @@
 // Created by darren on 31/03/2021.
 //
 
-#include <iostream>
+#include <cassert>
 #include "types.h"
+
+using ::Z80::InterruptMode;
 
 std::string std::to_string(const Z80::Register16 & reg)
 {
     switch (reg) {
         case Z80::Register16::AF:
-            return "AF";
+            return "AF"s;
 
         case Z80::Register16::BC:
-            return "BC";
+            return "BC"s;
 
         case Z80::Register16::DE:
-            return "DE";
+            return "DE"s;
 
         case Z80::Register16::HL:
-            return "HL";
+            return "HL"s;
 
         case Z80::Register16::IX:
-            return "IX";
+            return "IX"s;
 
         case Z80::Register16::IY:
-            return "IY";
+            return "IY"s;
 
         case Z80::Register16::SP:
-            return "SP";
+            return "SP"s;
 
         case Z80::Register16::PC:
-            return "PC";
+            return "PC"s;
 
         case Z80::Register16::AFShadow:
-            return "AF'";
+            return "AF'"s;
 
         case Z80::Register16::BCShadow:
-            return "BC'";
+            return "BC'"s;
 
         case Z80::Register16::DEShadow:
-            return "DE'";
+            return "DE'"s;
 
         case Z80::Register16::HLShadow:
-            return "HL'";
+            return "HL'"s;
     }
 
-    std::cerr << "unrecognised register pair (Register16 enumerator = " << static_cast<std::uint16_t>(reg) << ")\n";
+    // unreachable code - someone's added a 16-bit register and not updated the function or type-punned an invalid value
+    // to a register
+    assert(false);
     return {};
 }
 
@@ -53,72 +57,92 @@ std::string std::to_string(const Z80::Register8 & reg)
 {
     switch (reg) {
         case Z80::Register8::A:
-            return "A";
+            return "A"s;
 
         case Z80::Register8::F:
-            return "F";
+            return "F"s;
 
         case Z80::Register8::B:
-            return "B";
+            return "B"s;
 
         case Z80::Register8::C:
-            return "C";
+            return "C"s;
 
         case Z80::Register8::D:
-            return "D";
+            return "D"s;
 
         case Z80::Register8::E:
-            return "E";
+            return "E"s;
 
         case Z80::Register8::H:
-            return "H";
+            return "H"s;
 
         case Z80::Register8::L:
-            return "L";
+            return "L"s;
 
         case Z80::Register8::IXH:
-            return "IXH";
+            return "IXH"s;
 
         case Z80::Register8::IXL:
-            return "IXL";
+            return "IXL"s;
 
         case Z80::Register8::IYH:
-            return "IYH";
+            return "IYH"s;
 
         case Z80::Register8::IYL:
-            return "IYL";
+            return "IYL"s;
 
         case Z80::Register8::I:
-            return "I";
+            return "I"s;
 
         case Z80::Register8::R:
-            return "R";
+            return "R"s;
 
         case Z80::Register8::AShadow:
-            return "A'";
+            return "A'"s;
 
         case Z80::Register8::FShadow:
-            return "F'";
+            return "F'"s;
 
         case Z80::Register8::BShadow:
-            return "B'";
+            return "B'"s;
 
         case Z80::Register8::CShadow:
-            return "C'";
+            return "C'"s;
 
         case Z80::Register8::DShadow:
-            return "D'";
+            return "D'"s;
 
         case Z80::Register8::EShadow:
-            return "E'";
+            return "E'"s;
 
         case Z80::Register8::HShadow:
-            return "H'";
+            return "H'"s;
 
         case Z80::Register8::LShadow:
-            return "L'";
+            return "L'"s;
     }
 
-    std::cerr << "unrecognised register (Register8 enumerator = " << static_cast<std::uint16_t>(reg) << ")\n";
+    // unreachable code - someone's added an 8-bit register and not updated the function or type-punned an invalid value
+    // to a register
+    assert(false);
+    return {};
+}
+
+std::string std::to_string(const InterruptMode & im)
+{
+    switch (im) {
+        case InterruptMode::IM0:
+            return "IM0"s;
+
+        case InterruptMode::IM1:
+            return "IM1"s;
+
+        case InterruptMode::IM2:
+            return "IM2"s;
+    }
+    
+    // unreachable code - someone's added a mode and not updated the function or type-punned an invalid value to a mode
+    assert(false);
     return {};
 }
--- src/z80/types.h
+++ src/z80/types.h
@@ -11,31 +11,33 @@
 
 namespace Z80
 {
+    /**
+     * A Z80 8-bit unsigned value.
+     */
     using UnsignedByte = std::uint8_t;
+
+    /**
+     * A Z80 16-bit unsigned value.
+     *
+     * Note that values of this type do not guarantee Z80 byte order.
+     */
     using UnsignedWord = std::uint16_t;
+
+    /**
+     * A Z80 8-bit signed value.
+     */
     using SignedByte = std::int8_t;
+
+    /**
+     * A Z80 16-bit signed value.
+     *
+     * Note that values of this type do not guarantee Z80 byte order.
+     */
     using SignedWord = std::int16_t;
 
-    inline constexpr UnsignedByte operator "" _z80ub(unsigned long long value) noexcept
-    {
-        return static_cast<UnsignedByte>(value);
-    }
-
-    inline constexpr SignedByte operator "" _z80sb(unsigned long long value) noexcept
-    {
-        return static_cast<SignedByte>(value);
-    }
-
-    inline constexpr UnsignedWord operator "" _z80uw(unsigned long long value) noexcept
-    {
-        return static_cast<UnsignedWord>(value);
-    }
-
-    inline constexpr SignedWord operator "" _z80sw(unsigned long long value) noexcept
-    {
-        return static_cast<SignedWord>(value);
-    }
-
+    /**
+     * Enumeration of the available Z80 16-bit register pairs.
+     */
     enum class Register16 : std::uint8_t
     {
         AF, BC, DE, HL,
@@ -44,6 +46,11 @@ namespace Z80
         AFShadow, BCShadow, DEShadow, HLShadow
     };
 
+    /**
+     * Enumeration of the available Z80 8-bit registers.
+     *
+     * Note that some of these can't be addressed directly, but they physically exist.
+     */
     enum class Register8 : std::uint8_t
     {
         A, F, B, C, D, E, H, L,
@@ -52,6 +59,9 @@ namespace Z80
         AShadow, FShadow, BShadow, CShadow, DShadow, EShadow, HShadow, LShadow
     };
 
+    /**
+     * Enumeration of the possible Z80 interrupt modes.
+     */
     enum class InterruptMode : std::uint8_t
     {
         IM0 = 0,
@@ -69,11 +79,87 @@ namespace Z80
     constexpr const std::endian Z80ByteOrder = std::endian::little;
 }
 
-
-namespace std
+namespace std // NOLINT(cert-dcl58-cpp) only to_string() is overloaded and only with our namespaced types
 {
+    /**
+     * Provide a string representation of a 16-bit register pair name.
+     *
+     * @return
+     */
     std::string to_string(const ::Z80::Register16 &);
+
+    /**
+     * Provide a string representation of an 8-bit register name.
+     *
+     * @return
+     */
     std::string to_string(const ::Z80::Register8 &);
+
+    /**
+     * Provide a string representation of a interrupt mode.
+     * @return
+     */
+    std::string to_string(const ::Z80::InterruptMode &);
+}
+
+/**
+ * User-defined literal for Z80::UnsignedByte
+ *
+ * E.g., 0xff_z80ub is a Z80::UnsignedByte literal (value 255)
+ *
+ * @param value
+ * @return
+ */
+constexpr Z80::UnsignedByte operator "" _z80ub(unsigned long long value) noexcept
+{
+    return static_cast<Z80::UnsignedByte>(value);
+}
+
+/**
+ * User-defined literal for Z80::SignedByte
+ *
+ * E.g., 0xff_z80sb is a Z80::UnsignedByte literal (value -127)
+ *
+ * @param value
+ * @return
+ */
+constexpr Z80::SignedByte operator "" _z80sb(unsigned long long value) noexcept
+{
+    return static_cast<Z80::SignedByte>(value);
+}
+
+/**
+ * User-defined literal for Z80::UnsignedWord
+ *
+ * E.g., 0xffff_z80sb is a Z80::UnsignedWord literal (value 65535)
+ *
+ * Note that no byte-order conversion is performed, this is simply a way of ensuring a numeric literal value is cast to
+ * the underlying type being used for 16-bit Z80 values without having to either know the underlying type or write an
+ * explicit cast. The value will remain in host byte order.
+ *
+ * @param value
+ * @return
+ */
+constexpr Z80::UnsignedWord operator "" _z80uw(unsigned long long value) noexcept
+{
+    return static_cast<Z80::UnsignedWord>(value);
+}
+
+/**
+ * User-defined literal for Z80::SignedWord
+ *
+ * E.g., 0xffff_z80sb is a Z80::SignedWord literal (value -32767)
+ *
+ * Note that no byte-order conversion is performed, this is simply a way of ensuring a numeric literal value is cast to
+ * the underlying type being used for 16-bit Z80 values without having to either know the underlying type or write an
+ * explicit cast. The value will remain in host byte order.
+ *
+ * @param value
+ * @return
+ */
+constexpr Z80::SignedWord operator "" _z80sw(unsigned long long value) noexcept
+{
+    return static_cast<Z80::SignedWord>(value);
 }
 
 #endif //Z80_TYPES_H
