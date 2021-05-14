cefdc91196ae1200ddd0227212dd3f00fe323920
Darren Edale <darren@equituk.net>
2021-05-14 18:18:14 +0100

    Updated docs for DisassemblyWidget
    - no code changes, only comments

--- src/spectrum/qtui/debugger/disassemblywidget.h
+++ src/spectrum/qtui/debugger/disassemblywidget.h
@@ -6,7 +6,6 @@
 #define SPECTRUM_QTUI_DEBUGGER_DISASSEMBLYWIDGET_H
 
 #include <QScrollArea>
-
 #include "../../basespectrum.h"
 #include "../../../z80/assembly/disassembler.h"
 
@@ -14,6 +13,11 @@ namespace Spectrum::QtUi::Debugger
 {
     /**
      * A widget to show the disassembly of a bunch of Z80 machine code.
+     *
+     * The widget inherits from QScrollArea and provides its own internal widget that the QScrollArea renders. The takeWidget() and setWidget() methods are
+     * explicitly deleted since the widget is a private implementation detail for instances of this class and client code must not change it. Client code may
+     * safely call widget() to fetch a pointer to the internal widget, but its type is opaque and therefore it cannot be manipulated or queried in much of a
+     * meaningful way. Under no circumstances should the internal widget be re-parented.
      */
     class DisassemblyWidget
     : public QScrollArea
@@ -21,18 +25,55 @@ namespace Spectrum::QtUi::Debugger
         Q_OBJECT
 
     public:
+        /**
+         * Initialise a new disassembly widget with a memory object.
+         *
+         * The memory object is borrowed. It is the responsibility of the calling code to ensure that the widget does not retain a reference to it after it has
+         * been destroyed.
+         */
         explicit DisassemblyWidget(BaseSpectrum::MemoryType * = nullptr, QWidget * = nullptr);
 
+        /**
+         * Initialise a new disassembly widget with the memory from a Spectrum.
+         *
+         * The memory from the Spectrum is borrowed. It is the responsibility of the calling code to ensure that the widget does not retain a reference to it
+         * after it has been destroyed.
+         */
         explicit DisassemblyWidget(const BaseSpectrum & spectrum, QWidget * parent = nullptr)
         : DisassemblyWidget(spectrum.memory(), parent)
         {}
 
+        /**
+         * DisassemblyWidgets cannot be copy-constructed.
+         */
         DisassemblyWidget(const DisassemblyWidget &) = delete;
+
+        /**
+         * DisassemblyWidgets cannot be move-constructed.
+         */
         DisassemblyWidget(DisassemblyWidget &&) = delete;
+
+        /**
+         * DisassemblyWidgets cannot be copy assigned.
+         */
         void operator=(const DisassemblyWidget &) = delete;
+
+        /**
+         * DisassemblyWidgets cannot be move assigned.
+         */
         void operator=(DisassemblyWidget &&) = delete;
+
+        /**
+         * Destructor.
+         */
         ~DisassemblyWidget() override;
 
+        /**
+         * Set the memory to disassemble.
+         *
+         * The memory is borrowed. It is the responsibility of the calling code to ensure that the widget does not retain a reference to it after it has been
+         * destroyed.
+         */
         void setMemory(BaseSpectrum::MemoryType *);
 
         /**
@@ -86,16 +127,29 @@ namespace Spectrum::QtUi::Debugger
         /**
          * Show or hide the indicator for the current PC.
          *
-         * @param enabled
+         * This does not automatically track the PC of the Spectrum whose memory is borrowed. It simply switches on or off the visualisation. In order to ensure
+         * the correct address is visualised for the PC, client code must call setPc() with the correct address to update the view.
+         *
+         * @param enabled Whether or not the PC indicator should be enabled.
          */
         void enablePcIndicator(bool enabled = true);
 
+        /**
+         * Switch off the indicator for the current PC in the view.
+         */
         void disablePcIndicator()
         {
             enablePcIndicator(false);
         }
 
+        /**
+         * Explicitly delete the takeWidget() method inherited from QScrollArea. Client code must not alter the contained widget.
+         */
         QWidget * takeWidget() = delete;
+
+        /**
+         * Explicitly delete the setWidget() method inherited from QScrollArea. Client code must not alter the contained widget.
+         */
         void  setWidget(QWidget *) = delete;
     };
 }
