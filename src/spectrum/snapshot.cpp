bb428970ccd694f48b06b02b10a8dfa7b94c104b
Darren Edale <darren.edale@equit.dev>
2026-09-20 23:08:21 +0100

    Fix debug-only code in snapshot.cpp

--- src/spectrum/snapshot.cpp
+++ src/spectrum/snapshot.cpp
@@ -201,7 +201,7 @@ std::ostream & Spectrum::operator<<(std::ostream & out, const Snapshot & snap)
             }
         }
 
-        auto * memory = dynamic_cast<const PagingMemoryInterface *>(snap.memory());
+        auto * memory = dynamic_cast<const Memory::PagingMemoryInterface *>(snap.memory());
         assert(memory);
 
         for (std::uint8_t page = 0; page < 8; ++page) {
